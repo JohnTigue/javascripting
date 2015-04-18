504caeb409151f04c872439d74bb0487f49d8c10
John Tigue <john@tigue.com>
2015-04-18 00:17:37 -0700

    Cleaned up comments, variable names, and changed results accumulator.

--- src/explorations/promise/compositions.js
+++ src/explorations/promise/compositions.js
@@ -1,6 +1,7 @@
 /* global describe, console, require, before, beforeEach, after, afterEach, it */
 
 /** This file is a collection of explorations of composing Promises.
+  *
   * For basic single Promise explorations of how mocha interacts with
   * Promises, see: 
   * ../mocha/promise.js
@@ -22,6 +23,7 @@ describe('compositions.js:', function(){
 
   var Promise = require('bluebird');
 
+
   /* Basic sanity check:
    */
   context('when insta-resolving a Promise in before()', function(){
@@ -37,68 +39,73 @@ describe('compositions.js:', function(){
       });
     });
 
-  /* A simple construct for how to serially execute a series of tasks.
+
+  /* A simple Promises-based construct for how to serially execute a
+   * series of async tasks.
    *
    * Check out this conversation:
    * http://derickbailey.com/2015/03/12/serially-iterating-an-array-asynchronously/
    *
    * Someone, in the comments cranked out an elegant solutions with
-   * Promises serially chained via reduce(): 
+   * Promises serially chained via reduce(). That was the basis for the
+   * solution worked out in this context(): 
    * http://jsbin.com/bikiba/10/edit?js,console 
    * 
    * Basically, using Array.prototype.reduce() to take an Array of
-   * functions (zero arity "tasks") and chain their execution together
-   * into a sequentially executed series of Promises. If any one
-   * Promise gets rejected, then the chain stops halting and the rest
-   * of the functions/tasks are not executed.
+   * functions which are async tasks that need to be performed, one at
+   * a time, and chain their execution together into a sequentially
+   * executed series of Promises. If any one Promise gets rejected,
+   * then the chaining halts and the rest of the functions/tasks
+   * are not executed.
    */
   context('When given an Array of tasks to perform', function(){
-    var someEventEmittingTasks = null;
-    var recordOfResults = null;
-    var anEventRecord = [];
     var whatWereTheResults = null;
 
-    function kickOff(aTask, reportTo){
+    function kickOff(aTask, recordTo){
       // JFT-TODO: Perhaps a good place for Promise.promisify:
       // http://derickbailey.com/2015/03/12/serially-iterating-an-array-asynchronously/#comment-1906542548
       return new Promise(function(chooseResolve, chooseReject){
-        aTask(reportTo, function(err){
+        aTask(recordTo, function(err){
           if(err){
             chooseReject(err);
             }
             else{
-              chooseResolve(reportTo);
+              chooseResolve(recordTo);
               }
           });   
         });   
       }
 
-    // JFT-TODO: questioning the value of passing in aResultsRecorder; perhaps it should just be set int the init Promise.resolve()
-    function seriallyExecute(someTasks, aResultsRecorder){
-      //return recordOfResults = Promise.reject('UNIMPLD: promiseToSeriallyExecute');
-      return someTasks.reduce(function(soFar, nowDoThis){
-        return soFar.then(function(){
-          return kickOff(nowDoThis,anEventRecord);
+    function seriallyExecute(someTasks){
+      return someTasks.reduce(function(chainedPromises, nowDoThis){
+        return chainedPromises.then(function(resultsAccumulator){
+          return kickOff(nowDoThis, resultsAccumulator);
           });
         },
-        Promise.resolve()
+        Promise.resolve([]) 
         );      
       } 
 
+    /* This test is stuctured in a style wherein before() drives the
+     * SUT to the state at which to be tested. In this style it() just
+     * does asserts, it does not change the state of SUT. Mocha will
+     * wait for the Promise returned by before to resolve before calling
+     * it.
+     */
     before(function(){ 
-      someEventEmittingTasks = ['a','b','c','d'].map(function(i){
-        return function(anEventRecorder, callback){
+      var someAsyncNoteGeneratingTasks = ['a', 'b', 'c', 'd'].map(function(i){
+        return function(aNoteArchive, callback){
 	  var someMillis = Math.floor(Math.random() * 100);
 	  logger.debug('async task '+i+' is starting');
           var onCompleted = function(){
 	    logger.debug('async task '+i+' completed');
-            anEventRecorder.push(i);
+            aNoteArchive.push(i);
             callback();
 	    };
           setTimeout(onCompleted, someMillis);
 	  };
 	});
-      return whatWereTheResults = seriallyExecute(someEventEmittingTasks, anEventRecord);
+      return whatWereTheResults = seriallyExecute(someAsyncNoteGeneratingTasks);
       });
 
     it('must execute them serially', function(){
@@ -109,4 +116,3 @@ describe('compositions.js:', function(){
       });
     });
   });
-  
