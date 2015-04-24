07065d80edad639360f8c3286374e46523b58abf
John Tigue <john@tigue.com>
2015-04-23 22:19:30 -0700

    General clean up and better comments

--- src/explorations/javascript/bind.js
+++ src/explorations/javascript/bind.js
@@ -1,16 +1,14 @@
-/* JFT-TODO: probably want to call this fpification-via-bind-adaptors.js or such */
-
 'use strict';
 
-console.log('--------------');
 /* For the motivation behind this funky metaprogramming, check out 
- *  Braithwaite's (@raganwald) article on adaptors, The Symmetry of 
- *  JavaScript Functions:
- *  http://raganwald.com/2015/03/12/symmetry.html
+ * Braithwaite's (@raganwald) article on adaptors, The Symmetry of 
+ * JavaScript Functions:
+ * http://raganwald.com/2015/03/12/symmetry.html
  */
  
 
-/* A specific example can be found on MDN:
+/*****************************************
+ * A specific example can be found on MDN:
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Example:_Creating_shortcuts
  */
 
@@ -22,32 +20,36 @@ slice.call(arguments); // turns arguments into an array
 // same as "slice" in the previous example
 var unboundSlice = Array.prototype.slice;
 try{
-// Following link says next line of code should throw yet...
-// https://variadic.me/posts/2013-10-22-bind-call-and-apply-in-javascript.html
-console.log('About to throw, right?')
-var wat = unboundSlice(0,4); 
-//should have => TypeError: can't convert undefined to object. JFT-TODO: yet, node has no problem with this
-//console.log('wat:'+wat);
-console.log('Why didn\'t that throw???'); 
-}
-catch(e){
-console.log('Yup, it threw');
-}
-
+  // Following link says next line of code should throw yet not so in node
+  // https://variadic.me/posts/2013-10-22-bind-call-and-apply-in-javascript.html
+  console.log('About to throw, right?')
+  var wat = unboundSlice(0,4); 
+  //should have => TypeError: can't convert undefined to object. JFT-TODO: yet, node has no problem with this
+  console.log('Why didn\'t a throw just happen'); 
+  }
+  catch(e){
+    console.log('Yup, it threw');
+    }
+  
 // This is how to use bind to make it a function, not a method, yet will still have something to call 'this'
 var slice = Function.prototype.call.bind(unboundSlice);
 // ...
 slice(arguments);
 
-/* More generally, this ca be done with any Foo.prototype.doBar 
- * with the result being that the method can now be called as a function
- * where the first argument to the new form will be what would previously
- * have been the object that doBar would have been invoked as a method
- * So the form x.y(z) is now y(x,z) but cannot be y(z,x) because of the
- * specifics of how call() works. But that is sufficient to be useful to
- * get the method into a form where it can be used in FP style programming.
- * And in that space combinators could be used to create y(z,x) or many
- * other possibilities.
+
+/********************************************************************
+ * More generally, this can be done with any Foo.prototype.doBar with
+ * the result being that the method can now be called as a function
+ * where the first argument to the new form will be what would
+ * previously have been the object on which doBar would have been
+ * invoked as a method.
+ * 
+ * So the form x.y(z) becomes y(x,z). Note, it cannot become y(z,x)
+ * because of the specifics of how call() works. But what is possible
+ * is sufficient in order to get the method into a form where it can
+ * be used in FP style programming. Once that has been realized then
+ * in that form combinators (adaptors) could be used to create y(z,x),
+ * amongst many other possibilities.
  */
 function functionize(aMethod){
   return Function.prototype.call.bind(aMethod);
@@ -55,8 +57,8 @@ function functionize(aMethod){
 var slice = functionize(Array.prototype.slice);
 
 
-/* The bestest example of the same trick that will warm the hearts of
- * the FP folks (because bind() if fundimental to FP) is almost
+/* The bestest example of the same trick -- that will warm the hearts of
+ * the FP folks (because bind() is fundimental to FP) -- is almost
  * recursively seft referencial: functionize bind() itself, as shown
  * in:
  * https://variadic.me/posts/2013-10-22-bind-call-and-apply-in-javascript.html
