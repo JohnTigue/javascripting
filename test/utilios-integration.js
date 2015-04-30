54fbac63fa639f34138d073bfee62c7e49f11261
John Tigue <john@tigue.com>
2015-04-30 02:18:47 -0700

    Cleaned up the comments, bit time.

--- test/utilios-integration.js
+++ test/utilios-integration.js
@@ -1,12 +1,16 @@
 /* global console, require, describe, context, before, beforeEach, after, afterEach, it */
 
-/** This is a curious case. I don't really know where to put this code. 
-  * Since this whole nodeio repo is for me kicking around node and such,
-  * well this code is being put here in the nodeio repo because "and such"
-  * includes npm. What needs to be test is I have modules wired up properly.
-  * I want to do that short of publishing a package on the npm system. Fortunately,
-  * npm can install dependencies via GitHub URLs, so if you look in this
-  * repos package.json you will find the follow bit:
+/** This is a curious case. I don't really know where to put this
+  * code. But since this whole nodeio repo is for me kicking around node
+  * and such, well, this file of code is being put here in the nodeio repo
+  * because the "and such" part includes kicking the tires of npm itself. 
+  * 
+  * What needs to be tested is if I have npm packaged modules wired up
+  * properly. I want to do that short of actually publishing a package
+  * on the npm system (`npm publish`). Fortunately, npm can install
+  * package.json specified dependencies via GitHub URLs. 
+  * 
+  * In this repos package.json can be found the following bit:
   *   "dependencies": {
   *      "bluebird": "^2.9.24",
   *      "bunyan": "^1.3.5",
@@ -14,17 +18,21 @@
   *      "utilios": "git://github.com/JohnTigue/utilios.git"
   *      }
   *
-  * That last one is the unusual bit. Notice how it is a git:// URL rather
-  * than simply a npm semver spec. `npm install` will grap that git:// URL and
-  * park its contents in this repo's node_modules directory along with the
-  * other 3 dependencies. (Notice how this git URL spec provides a greater 
-  * level of naming freedom compared to the usual way of having to name dependencies 
-  * using their "global" name in the npm system. But since this is a test run of
-  * how a package would be used in npm and that package would be named "utilios"
-  * then is seems like a good idea to arbitrarily name it "utilios when fetched
-  * via git:// rather than through the npm package libary system. [ugh, so wordy])
+  * That last key:value is the unusual bit. Notice how it is a git://
+  * URL rather than simply a npm semver spec. `npm install` will grab
+  * that git:// URL and park its contents in this repo's node_modules
+  * directory, just like it will do with the other 3 npm-semver-spec'd
+  * dependencies. 
+  *
+  * Notice how a git URL spec provides a greater level of naming
+  * freedom compared to the usual way of having to name dependencies
+  * using their "global" name in the npm system. But since this is a
+  * test run of how a packaged module would be used as installed
+  * through npm and that package would be named "utilios" then is
+  * seems like a good idea to name its key "utilios" since in the
+  * JavaScript code it will included via a `require("utilios")`.
   * 
-  * Furthermore, that freedom really isn't there if using npm CLI to install:
+  * Furthermore, that freedom really isn't there if using npm's CLI to install:
   *   `npm install JohnTigue/utilios --save`	
   * That will update package.json as follows:
   *   "dependencies": {
@@ -57,6 +65,19 @@ describe('utilios-integration.js', function(){
       it('the module should have its sub-modules a properties', function(){
         must(utilios.logger).exist();
         });
+      });
+    });
+
+  
+
+  context.skip('BLANK', function(){
+    before(function(){
+      });
+
+    it('must BLANK', function(){
+      });
+  
+    after(function(){
       });
     });
   });
